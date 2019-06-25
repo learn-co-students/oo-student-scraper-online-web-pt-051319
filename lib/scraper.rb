1f4e1c995d236bf1a36c2a21aff4c251f58e8b9c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-25 02:10:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,7 +30,6 @@ class Scraper
                 elsif item.css("img").attr("src").value  == "../assets/img/github-icon.png"
                     profile[:github] = item.attr("href")
                 else
-                #if item.css("img").attr("src").value == "../assets/img/rss-icon.png"
                     profile[:blog] = item.attr("href")
                     end 
                     end 
